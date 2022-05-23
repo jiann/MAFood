1cdcb27d752fc0a12eddecbe25023bfb5b035d30
jw <jiannweyfcutw@mail.fcu.edu.tw>
2022-05-23 15:41:12 +0800

    store added

--- app/src/main/java/com/ma/mafood/MainActivity.java
+++ app/src/main/java/com/ma/mafood/MainActivity.java
@@ -4,18 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     public static boolean VALID_USER = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_testing_area);
 
-        if (!VALID_USER) {
-            Intent intent = new Intent();
-            intent.setClass(this, LoginActivity.class);
-            startActivity(intent);
-        }
+        //if (!VALID_USER) {
+//            Intent intent = new Intent();
+//            intent.setClass(this, TestingAreaActivity.class);
+//            startActivity(intent);
+        //}
+    }
+    public void gotoMainActivity(View v) {
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+    }
+    public void gotoLoginActivity(View v) {
+        Intent it = new Intent(this, LoginActivity.class);
+        startActivity(it);
+    }
+    public void gotoRegisterActivity(View v) {
+        Intent it = new Intent(this, RegisterActivity.class);
+        startActivity(it);
+    }
+    public void gotoStoreActivity(View v){
+        Intent it = new Intent(this, StoreActivity.class);
+        startActivity(it);
+    }
+    public void goBack(View v) {
+        finish();
     }
 }
---
+++ app/src/main/java/com/ma/mafood/StoreActivity.java
@@ -0,0 +1,16 @@
+package com.ma.mafood;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
